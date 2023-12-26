3856e308926a7b08451e5c46cf01da45405892a8
Vladimir_Glazyrin <crv59@yandex.ru>
2023-12-26 20:22:16 +0500

    Сделали 4 задачу

--- HW_8/1_4/Program.cs
+++ HW_8/1_4/Program.cs
@@ -1,4 +1,14 @@
 ﻿//Задача 4: Напишите программу, которая на вход принимает натуральное число N, 
 //а на выходе показывает его цифры через запятую.
 
+string no = "8965"; 
+string [] numberArray = new string[no.Length]; 
+int counter = 0; 
 
+    for (int i = 0; i < no.Length; i++) 
+    { 
+    numberArray[i] = no.Substring(counter, 1); 
+    counter++; 
+    } 
+
+Console.WriteLine(string.Join(" , ", numberArray)); 
