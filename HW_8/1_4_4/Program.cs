f1c2e2ec95dfc1b25a0cae3ef5600e9aba31d379
Vladimir_Glazyrin <crv59@yandex.ru>
2023-12-27 19:50:11 +0500

    Сделали задачу 4, второе решение

---
+++ HW_8/1_4_4/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает натуральное число N, 
+//а на выходе показывает его цифры через запятую.
+
+//ВТОРОЕ РЕШЕНИЕ
+
+Console.WriteLine("Enter Number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+            int result = 0;
+            int y = number.ToString().Length;
+ 
+            while (number > 0)
+            {
+                result *= 10;
+                result += number % 10;
+                number /= 10;
+            }
+ 
+            for (int i = 0; i < y; i++)
+            {
+                Console.Write(result%10+ " , ");
+                result /= 10;
+            }
+ 
