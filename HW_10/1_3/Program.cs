3d26559d50fd74f92e87fccd41bb23a966af2d01
Vladimir_Glazyrin <crv59@yandex.ru>
2024-01-17 09:11:39 +0500

    ДЗ_9 задача 3

---
+++ HW_10/1_3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 3: Напишите программу, которая перевернёт одномерный массив 
+//(первый элемент станет последним, второй – предпоследним и т.д.)
+
+
+class Program
+{
+        static void Main(string[] args)
+        {
+                int[] myArray = { 1, 10, 0, -5 };
+                Console.WriteLine("\n Вывод массива: ");
+                for (int i = myArray.Length - 1; i >= 0; i--)
+                {
+                        Console.WriteLine(myArray[i]);
+                }
+                Console.ReadLine();
+        }
+}
