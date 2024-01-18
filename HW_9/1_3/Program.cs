bfb90e7a54128525d5c298f58922f44203c979d9
Vladimir_Glazyrin <crv59@yandex.ru>
2024-01-18 09:03:49 +0500

    ДЗ_9 автотесты

---
+++ HW_9/1_3/Program.cs
@@ -0,0 +1,66 @@
+﻿﻿// Задайте массив из вещественных чисел с ненулевой дробной частью. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+//Начальные условия:
+
+//int[] array = {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
+//Выводится: 5.15
+
+
+using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    // Разница между максимальным и минимальным элементами массива
+    
+    // Нахождение минимума массива
+    public static double FindMin(double[] numbers)
+    {
+         return numbers.Min();
+    }
+    
+    // Нахождение максимума массива
+    public static double FindMax(double[] numbers)
+    {
+        return numbers.Max();
+    }
+
+    // Находим разницу между максимальным и минимальным элементами
+   
+    public static void PrintResult(double[] array)
+    {
+      double min = FindMin(array);
+        double max = FindMax(array);
+        double diff = max - min;
+      
+      Console.WriteLine($"{diff}");
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        double[] array;
+        
+
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(double.Parse)
+                                  .ToArray();
+            
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+        } else {
+           // Если аргументов на входе нет
+            array = new double[] {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
