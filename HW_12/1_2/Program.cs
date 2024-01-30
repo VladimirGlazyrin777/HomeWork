d6412e696547bb987bdf7b44a559d6b82ad5717a
Vladimir_Glazyrin <crv59@yandex.ru>
2024-01-30 20:11:33 +0500

    HW_12\Task 1_2

---
+++ HW_12/1_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+//Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+using System;
+ 
+public class Example
+{
+    public static void Main()
+    {
+        string s = "Hello WoRld";
+ 
+        s = s.ToLower();
+ 
+        Console.WriteLine(s);        // привет мир
+    }
+}
+
+
