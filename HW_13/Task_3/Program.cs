8ecae26500831ad9cc83571cfe473b3d1a6c27ad
Vladimir_Glazyrin <crv59@yandex.ru>
2024-02-07 22:19:33 +0500

    HW_13\Task 3

--- HW_13/Task_3/Program.cs
+++ HW_13/Task_3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 3: Задайте произвольный массив. 
+//Выведете его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+class program
+{
+static void print(int[] array, int first = 0)
+{
+    if(first != array.Length) {
+        print(array, first + 1);
+        Console.Write($"{array[first]} ");
+    }
+}
+        
+ static void Main(string[] args)
+{
+    var array = new int[]{1, 2, 5, 0, 10, 34 };
+    print(array);            
+}
+
+}
